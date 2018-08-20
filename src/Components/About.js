76d4702ba5914b87c711cc32e1797922bec08257
Finskou <lelarhaz@gmail.com>
2018-08-20 15:20:25 +0200

    package + css + section update

--- src/Components/About.js
+++ src/Components/About.js
@@ -25,7 +25,7 @@ class About extends Component {
             <h2>About Me</h2>
 
             <p>{bio}</p>
-            <div className="row">
+            {/* <div className="row">
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
@@ -34,10 +34,10 @@ class About extends Component {
 						         {city} {state}, {zip}
                    </span><br />
 						   <span>{phone}</span><br />
-                     <span>{email}</span>
+                     <span>{email}</span> 
 					   </p>
                </div>
-            </div>
+            </div>*/}
          </div>
       </div>
 
