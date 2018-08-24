f695cb2a3b5ff7e4322d98b93e8f689cee892093
Finskou <lelarhaz@gmail.com>
2018-08-24 12:25:34 +0200

    try init

--- src/index.js
+++ src/index.js
@@ -6,3 +6,10 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+var http = require('http')
+
+http.createServer(function (request, response){
+    response.writeHead(200, {"Content-Type": "text/plain"})
+    response.end("Hello World\n")
+}).listen(process.env.PORT)
